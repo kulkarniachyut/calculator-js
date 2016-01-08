83d519785e61e694dba536a26201b053d4061ab7
achi <achyut.gk@gmail.com>
2016-01-07 19:47:13 -0800

    new

--- calculator.js
+++ calculator.js
@@ -19,21 +19,10 @@ function num(numberValue){
         }
 }
 
-
-function res(){
+function findLeftDigits( a , leftDigitArray){
     
-    var a = digitSymArray.indexOf("*");
-    var leftDigitArray = [];
-//    var rightDigitArray = [];
-    
-     if(isNaN(digitSymArray[a-1]) || isNaN(digitSymArray[a+1])){
-        console.log("Not a correct number inserted");
-        } 
-    
-    else {
-        
-         for (var i = 1; i< digitSymArray.length ; i++){
-//                var leftIndex = digitSymArray.indexOf("*"||"+"||"-"||"/");
+     for (var i = 1; i< digitSymArray.length ; i++){
+//               
                 
                 if(isNaN(digitSymArray[a-i])){
                     
@@ -47,7 +36,7 @@ function res(){
                         
                     }
                    
-//                    console.log(leftDigit);
+//                    
                 }
              
              
@@ -55,12 +44,67 @@ function res(){
 //                    console.log(rightDigitArray);
                 
           }
+    
             console.log(leftDigitArray);
             leftDigit = parseInt((leftDigitArray).join(""));
             console.log(leftDigit);
+            return leftDigit ;
+    
+    
+    
+}
+
+
+
+function findRightDigits(a,rightDigitArray){
+        
             
+    //console.log(a);
+     //console.log(digitSymArray.length);
+     for (var i = a+2; i<(digitSymArray.length) ; i++){
+//               
+//                console.log(digitSymArray[a+i]);
+              if(isNaN(digitSymArray[i])){
+                 console.log("found a symbol");
+                 rightDigitArray = digitSymArray.slice(a+1,i);
+                 }
+               if ( i ==((digitSymArray.length)-1) ){
+                   
+                   rightDigitArray = digitSymArray.slice(a+1,(digitSymArray.length)+1);
+               }
+//                  
+                
+          }
+//    
+            console.log(rightDigitArray);
+            rightDigit = parseInt((rightDigitArray).join(""));
+            console.log(rightDigit);
+            return rightDigit ;
+    
+}
+function clear(){ 
+
+   document.getElementById("result").innerHTML = "cleared";
+}
+
+
+
+function res(){
+    
+    var a = digitSymArray.indexOf("*");
+    var leftDigitArray = [];
+    var rightDigitArray = [];
+    
+     if(isNaN(digitSymArray[a-1]) || isNaN(digitSymArray[a+1])){
+        console.log("Not a correct number inserted");
+        } 
+    
+    else {
         
-        
+        var leftDigit = findLeftDigits(a,leftDigitArray);
+        var righDigit = findRightDigits(a,rightDigitArray);
+        var res = leftDigit * righDigit;
+        document.getElementById("result").innerHTML = res;
         }
        
     
